ced9cee0c19cafb20ff6859b5802e0ca57f2d7b3
Peng Lu <penglu2097@gmail.com>
2018-07-26 20:45:08 +0800

    update

--- models/__init__.py
+++ models/__init__.py
@@ -3,13 +3,7 @@ import os
 sys.path.append(os.path.abspath(__file__).rsplit('/',1)[0])
 
 
-import Encoder
-Encoder = Encoder.Encoder_asmb
-import Decoder
-Decoder = Decoder.Decoder_asmb
-import Discriminator
-Discriminator = Discriminator.Discriminator_asmb
-
-import losses
-TripletLoss = losses.TripletLoss
-#__all__ = ['Encoder', 'Decoder', 'Discriminator']
+from .Encoder import Encoder_asmb as Encoder
+from .Decoder import Decoder_asmb as Decoder
+from .Discriminator import Discriminator_asmb as Discriminator
+from .losses import *
